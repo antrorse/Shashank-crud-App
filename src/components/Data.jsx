ba5e557f15c0b23a99a909a0f9800b7c3cb8b60f
Shashank-Reddy-009 <150832913+antrorse@users.noreply.github.com>
2023-11-25 16:22:42 +0530

    Initial Commit

---
+++ src/components/Data.jsx
@@ -0,0 +1,12 @@
+export const data=[
+    {
+        name:"Shashank",
+        email:"mail@Gmail.com",
+        id:1
+    },
+    {
+        name:"Masthan",
+        email:"mastan@Gmail.com",
+        id:2
+    }
+]
